dacf286c1c7376c2198c0f4a295333a01bb77079
vp4321 <56974948+vp4321@users.noreply.github.com>
2021-05-15 23:38:13 +0530

    Initial Commit

---
+++ models/article_model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+const marked = require('marked') 
+const slugify = require('slugify')
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        required: true,
+        type: String
+    },
+    description: {
+        required: true,
+        type: String
+    },
+    markdown: {
+        required: true,
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+
+    },
+    sanitizedHtml: {
+        type: String,
+        required: true
+    }
+})
+
+articleSchema.pre('validate', function (next) {
+    this.slug = slugify(this.title, { lower: true, strict: true })
+
+    if (this.markdown) {
+        this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+    }
+
+    next()
+})
+
+
+
+module.exports = mongoose.model('Article', articleSchema)
